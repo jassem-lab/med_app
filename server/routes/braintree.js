2dde4bcf084d4dade02b73708d7fff0aa05be231
Jassem Gaaloul <jassemgaaloul123@gmail.com>
2020-12-23 21:46:38 +0100

    second structure update

--- server/routes/braintree.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const brainTreeController = require('../controller/braintree');
-
-router.post('/braintree/get-token', brainTreeController.ganerateToken);
-router.post('/braintree/payment', brainTreeController.paymentProcess);
-
-module.exports = router;
